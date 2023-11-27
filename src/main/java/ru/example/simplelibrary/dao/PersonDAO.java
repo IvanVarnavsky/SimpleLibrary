6c39b5dbbe2c23c43edc5cdd1d7eee19142c5c89
Ivan Varnavsky <varnavskyivan@gmail.com>
2023-11-28 03:05:26 +0500

    a little cleaning

--- src/main/java/ru/example/simplelibrary/dao/PersonDAO.java
+++ src/main/java/ru/example/simplelibrary/dao/PersonDAO.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import ru.example.simplelibrary.models.Book;
 import ru.example.simplelibrary.models.Person;
 
 import java.util.List;
